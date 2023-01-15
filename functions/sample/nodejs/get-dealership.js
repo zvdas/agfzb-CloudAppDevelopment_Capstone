0694e0d952a20ba9e0d33aa07948c333ad094a61
zvdas <rodriguezzude@gmail.com>
2023-01-15 22:39:03 +0530

    get template added

---
+++ functions/sample/nodejs/get-dealership.js
@@ -0,0 +1,30 @@
+const { CloudantV1 } = require('@ibm-cloud/cloudant');
+const { IamAuthenticator } = require('ibm-cloud-sdk-core');
+
+function main(params) {
+
+    const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
+    const cloudant = CloudantV1.newInstance({
+      authenticator: authenticator
+    });
+    cloudant.setServiceUrl(params.COUCH_URL);
+
+    return getAllRecords(cloudant, "dealerships");
+}
+
+ /*
+ Sample implementation to get all the records in a db.
+ */
+ function getAllRecords(cloudant,dbname) {
+     return new Promise((resolve, reject) => {
+         cloudant.postAllDocs({ db: dbname, includeDocs: true, limit: 10 })            
+             .then((result)=>{
+              console.log(result.result.rows)
+              resolve({ result: result.result.rows });
+             })
+             .catch(err => {
+                console.log(err);
+                reject({ err: err });
+             });
+         })
+ }
