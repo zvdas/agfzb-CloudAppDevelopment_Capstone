30eafa24fc13ef0c82efe6c1cbc34d4c0ff38eab
zvdas <rodriguezzude@gmail.com>
2023-01-16 08:16:06 -0500

    get-review python 3.7 code added

--- functions/sample/python/get-review.py
+++ functions/sample/python/get-review.py
@@ -1,6 +1,8 @@
 from cloudant import Cloudant
 
 def main(dict):
+from cloudant.client import Cloudant
+
+def main(dict):
     client = Cloudant.iam(account_name=dict['ACCOUNT_NAME'], api_key=dict['API_KEY'], connect=True)
-    for document in client['reviews']:
-        print(document)
+    return {"reviews": [x for x in client['reviews']]}
