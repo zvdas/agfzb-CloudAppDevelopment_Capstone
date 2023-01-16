251c2d2712554e6ccebd1befbe1fcab7febc5a7e
zvdas <rodriguezzude@gmail.com>
2023-01-16 08:34:23 -0500

    post-review python updated"

--- functions/sample/python/post-review.py
+++ functions/sample/python/post-review.py
@@ -1,3 +1,12 @@
+#
+#
+# main() will be run when you invoke this action
+#
+# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+#
+# @return The output of this action, which must be a JSON object.
+#
+#
 # from cloudant import Cloudant
 from cloudant.client import Cloudant
 
@@ -6,14 +15,14 @@ def main(dict):
     # Create document content data
     data = {
         # "id": 1, # Setting _id is optional
-        "name": "John Doe",
-        "dealership": 15,
-        "review": "Total grid-enabled service-desk",
-        "purchase": true,
-        "purchase_date": "07/11/2020",
-        "car_make": "Audi",
-        "car_model": "A6",
-        "car_year": 2010
+        "name": dict['name'],
+        "dealership": dict['dealership'],
+        "review": dict['review'],
+        "purchase": dict['purchase'],
+        "purchase_date": dict['purchase_date'],
+        "car_make": dict['car_make'],
+        "car_model": dict['car_model'],
+        "car_year": dict['car_year']
         }
 
     # Create a document using the Database API
@@ -22,3 +31,17 @@ def main(dict):
     # Check that the document exists in the database
     if my_document.exists():
         return {"review": my_document}
+
+
+{
+    "ACCOUNT_NAME": "https://b65d1bed-d060-4cb3-91b4-ba3cbb0b7e16-bluemix.cloudantnosqldb.appdomain.cloud",
+    "API_KEY": "vIu9HaKiRhjDxioRa24NhIKdxETRuWRJDhP8_fU9FWfc",
+    "name": "John Doe",
+    "dealership": 15,
+    "review": "Total grid-enabled service-desk",
+    "purchase": true,
+    "purchase_date": "07/11/2020",
+    "car_make": "Audi",
+    "car_model": "A6",
+    "car_year": 2010
+}
