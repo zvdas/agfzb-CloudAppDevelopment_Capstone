2f35d5d266ea7f89a6c9cf85c9c51cc22f778bbc
zvdas <rodriguezzude@gmail.com>
2023-01-16 08:41:36 -0500

    post-review python updated"

--- functions/sample/python/post-review.py
+++ functions/sample/python/post-review.py
@@ -34,7 +34,7 @@ def main(dict):
 
 
 {
-    "ACCOUNT_NAME": "https://b65d1bed-d060-4cb3-91b4-ba3cbb0b7e16-bluemix.cloudantnosqldb.appdomain.cloud",
+    "ACCOUNT_NAME": "b65d1bed-d060-4cb3-91b4-ba3cbb0b7e16-bluemix",
     "API_KEY": "vIu9HaKiRhjDxioRa24NhIKdxETRuWRJDhP8_fU9FWfc",
     "name": "John Doe",
     "dealership": 15,
