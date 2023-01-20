a6b6dd7b87a756ee047d855db88e8f38db7c0704
zvdas <67303760+zvdas@users.noreply.github.com>
2023-01-20 20:27:01 +0530

    Update 01.get-review.py

--- functions/sample/python/01.get-review.py
+++ functions/sample/python/01.get-review.py
@@ -1,3 +1,16 @@
+# python 3.9
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+def main(dict):
+    authenticator = IAMAuthenticator(dict['API_KEY'])
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url(dict['COUCH_URL'])
+    response = service.post_all_docs(db='reviews',include_docs=True).get_result()
+    return response
+
+
+# python 3.7
 # from cloudant import Cloudant
 from cloudant.client import Cloudant
 
