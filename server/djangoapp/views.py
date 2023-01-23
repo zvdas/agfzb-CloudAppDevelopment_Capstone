4d9d276bc45924a15354aee9e9f10cbb97788e1f
zvdas <rodriguezzude@gmail.com>
2023-01-23 21:12:56 +0530

    get dealership code update

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -88,13 +88,10 @@ def get_dealerships(request):
         # Get dealers from the URL
         dealerships = get_dealers_from_cf(url)
         # Concat all dealer's short name
-        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
         context = {'dealership_list': [dealer for dealer in dealerships]}
         # Return a list of dealer short name
         # return HttpResponse(dealer_names)
-# def get_dealerships(request):
-#     context = {}
-#     if request.method == "GET":
         return render(request, 'djangoapp/index.html', context)
 
 
