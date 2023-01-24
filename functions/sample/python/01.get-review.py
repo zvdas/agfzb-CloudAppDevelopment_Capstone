bfceae8c62481ab723ab47353fb164fb639e0bb9
zvdas <67303760+zvdas@users.noreply.github.com>
2023-01-24 21:33:55 +0530

    Update 01.get-review.py

--- functions/sample/python/01.get-review.py
+++ functions/sample/python/01.get-review.py
@@ -9,6 +9,22 @@ def main(dict):
     response = service.post_all_docs(db='reviews',include_docs=True).get_result()
     return response
 
+# get review by id - python 3.9
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+def main(dict):
+    authenticator = IAMAuthenticator(dict['API_KEY'])
+    service = CloudantV1(authenticator=authenticator)
+    
+    service.set_service_url(dict['COUCH_URL'])
+    
+    response = service.post_find(
+      db='users',
+      selector={'dealership': {'$eq': dict['DEALERID']}},
+    ).get_result()
+    
+    return response
 
 # python 3.7
 # from cloudant import Cloudant
@@ -17,3 +33,9 @@ from cloudant.client import Cloudant
 def main(dict):
     client = Cloudant.iam(account_name=dict['ACCOUNT_NAME'], api_key=dict['API_KEY'], connect=True)
     return {"reviews": [x for x in client['reviews']]}
+
+{
+    "API_KEY": "",
+    "COUCH_URL": "",
+    "DEALERID": 
+}
